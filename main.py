4364b93b86462e04a89133ab742623bf61b919a8
Leaptail <52641374+Leaptail@users.noreply.github.com>
2024-02-22 20:54:18 +0800

    creating base lottery machine
    I want to add actual rolling slots later

---
+++ main.py
@@ -0,0 +1,85 @@
+import random
+
+
+MAX_LINES = 3
+MAX_BET = 100
+MIN_BET = 10
+
+ROWS = 3
+COLS = 3
+
+symbol_count = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8
+}
+
+def get_slot_machine_spin(rows,cols,symbols):
+    all_symbols = []
+    for symbol, symbol_count in symbols.items():
+        
+
+def deposit():
+    while True:
+        amount = input("How much would you like to deposit? $")
+        if amount.isdigit():
+            amount = int(amount)
+            if amount > 0:
+                break
+            else:
+                print("Amount must be greater than 0.")
+        else:
+            print("Please enter a valid number.")
+
+    return amount
+
+def get_number_of_lines():
+    while True:
+        lines = input("Enter the number of lines to bet on (1-" + str(MAX_LINES) + ")?")
+        if lines.isdigit():
+            lines = int(lines)
+            if 1 <= lines <= MAX_LINES:
+                break
+            else:
+                print("Amount must be greater than 1.")
+        else:
+            print("Please enter a valid number.")
+
+    return lines
+
+def get_bet():
+    while True:
+        bet = input("How much would you like to bet on each line (" + str(MIN_BET) + " to " + str(MAX_BET) + ")?")
+        if bet.isdigit():
+            bet = int(bet)
+            if MIN_BET <= bet <= MAX_BET:
+                break
+            else:
+                print(f"Amount must be between ${MIN_BET} to ${MAX_BET}")
+        else:
+            print("Please enter a valid number.")
+
+    return bet
+
+
+
+def main():
+    balance = deposit()
+    lines = get_number_of_lines()
+
+    while True:
+        bet = get_bet()
+        total_bet = bet * lines
+
+        if total_bet > balance:
+            print(f"You do not have enough to bet that amount, your current balance is ${balance}")
+        else:
+            break
+
+    print(f"You are betting ${bet} on {lines} lines. Total bet is equal to: ${total_bet}")
+
+    #print(balance, lines, bet)
+
+
+main()
